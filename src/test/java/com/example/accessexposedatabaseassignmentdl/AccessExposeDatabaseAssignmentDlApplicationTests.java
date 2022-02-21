37c692bffbdfd508d4a9fb353c73ea3809a31f60
DeMassou <98885191+DeMassou@users.noreply.github.com>
2022-02-21 13:51:05 +0100

    Initial commit

---
+++ src/test/java/com/example/accessexposedatabaseassignmentdl/AccessExposeDatabaseAssignmentDlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessexposedatabaseassignmentdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessExposeDatabaseAssignmentDlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
